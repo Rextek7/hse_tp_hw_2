57c2c3cb4c4909c0db82e851bb9d81790ec1ca7e
Rextek7 <113045888+Rextek7@users.noreply.github.com>
2022-10-08 21:54:47 +0300

    Update Homework_2.py

--- Homework_2.py
+++ Homework_2.py
@@ -4,45 +4,46 @@ print("Введите название файла:")
 while True:
     try:
         f = input()
+        with open(f) as file:
+            file_ = file.read().split(' ')
+        line = [int(item) for item in file_]
+
+        k = _sm = _mx = _del = 0
+        _mn = 10^8
+
+        start_time = datetime.now()
+
+        def _at_all(k):
+            for i in range(len(line)):
+                k += 1
+            return k
+
+        def _min(_mn):
+            for i in range(len(line)):
+                _mn = min(_mn, line[i])
+            return _mn
+
+        def _max(_mx):
+            _mx = max(line)
+            return _mx
+
+        def _sum(_sm):
+            _sm = sum(line)
+            return _sm
+
+        def _mult(line):
+            _mult = 1
+            for i in (line):
+                _mult = (_mult * i)
+            return _mult
+
+        def _delimost(_del):
+            _sum = sum(line)
+            _del = _sum % 3
+            return _del
     except EOFError:
         break
-with open(f) as file:
-    file_ = file.read().split(' ')
-line = [int(item) for item in file_]
 
-k = _sm = _mx = _del = 0
-_mn = 10^8
-
-start_time = datetime.now()
-
-def _at_all(k):
-    for i in range(len(line)):
-        k += 1
-    return k
-
-def _min(_mn):
-    for i in range(len(line)):
-        _mn = min(_mn, line[i])
-    return _mn
-
-def _max(_mx):
-    _mx = max(line)
-    return _mx
-
-def _sum(_sm):
-    _sm = sum(line)
-    return _sm
-
-def _mult(line):
-    _mult = 1
-    for i in (line):
-        _mult = (_mult * i)
-    return _mult
-
-def _delimost(_del):
-    _sum = sum(line)
-    _del = _sum % 3
-    return _del
 
 print("Количество чисел:", _at_all(k))
 print("Минимальное значение:", _min(_mn))
