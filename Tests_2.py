721c54c5992ef579530f24261e30b759fc7cce94
Rextek7 <113045888+Rextek7@users.noreply.github.com>
2022-10-11 22:04:03 +0300

    Update Tests_2.py

--- Tests_2.py
+++ Tests_2.py
@@ -1,9 +1,4 @@
-from Homework_2 import _max
-from Homework_2 import _min
-from Homework_2 import _sum
-from Homework_2 import _mult
-from Homework_2 import _delimost
-from Homework_2 import line
+import Homework_2
 import os
 import unittest
 from functools import reduce
@@ -11,25 +6,43 @@ from functools import reduce
 
 class TestCase(unittest.TestCase):
 
+    if os.stat('Numbers.txt').st_size != 0:
+
         def test_01_maximum(self):
-            test_max = _max(line)
-            self.assertEqual(test_max, max(line))
+            test_max = Homework_2._max(Homework_2.Homework_2.line)
+            self.assertEqual(test_max, max(Homework_2.Homework_2.line))
 
         def test_02_minimum(self):
-            test_min = _min(line)
-            self.assertEqual(test_min, min(line))
+            test_min = Homework_2._min(Homework_2.Homework_2.line)
+            self.assertEqual(test_min, min(Homework_2.Homework_2.line))
 
         def test_03_summa(self):
-            test_sum = _sum(line)
-            self.assertEqual(test_sum, sum(line))
+            test_sum = Homework_2._sum(Homework_2.Homework_2.line)
+            self.assertEqual(test_sum, sum(Homework_2.Homework_2.line))
 
         def test_04_multiply(self):
-            test_mult = _mult(line)
-            self.assertEqual(test_mult, reduce(lambda x, y: x * y, (line)))
+            test_mult = Homework_2._mult(Homework_2.Homework_2.line)
+            self.assertEqual(test_mult, reduce(lambda x, y: x * y, (Homework_2.Homework_2.line)))
 
         def test_05_del(self):
-            test_delimost = _delimost(line)
-            self.assertEqual(test_delimost, sum(line) % 3)
+            test_delimost = Homework_2._delimost(Homework_2.Homework_2.line)
+            self.assertEqual(test_delimost, sum(Homework_2.line) % 3)
+    else:
+        def test_02_min_on_empty_file(self):
+            self.assertIsNone(Homework_2._min(Homework_2.line))
+
+        def test_01_max_on_empty_file(self):
+            self.assertIsNone(Homework_2._max(Homework_2.line))
+
+        def test_03_summ_on_empty_file(self):
+            self.assertIsNone(Homework_2._sum(Homework_2.line))
+
+        def test_04_mult_on_empty_file(self):
+            self.assertIsNone(Homework_2._mult(Homework_2.line))
+
+        def test_05_del_on_empty_file(self):
+            self.assertIsNone(Homework_2._delimost(Homework_2.line))
+
 
 
 if __name__ == '__main__':
