63bacb7c123bf03f3039a93f2c31f9f95096ace0
Rextek7 <113045888+Rextek7@users.noreply.github.com>
2022-10-11 22:10:32 +0300

    Update Tests_2.py

--- Tests_2.py
+++ Tests_2.py
@@ -9,23 +9,23 @@ class TestCase(unittest.TestCase):
     if os.stat('Numbers.txt').st_size != 0:
 
         def test_01_maximum(self):
-            test_max = Homework_2._max(Homework_2.Homework_2.line)
-            self.assertEqual(test_max, max(Homework_2.Homework_2.line))
+            test_max = Homework_2._max(Homework_2.line)
+            self.assertEqual(test_max, max(Homework_2.line))
 
         def test_02_minimum(self):
-            test_min = Homework_2._min(Homework_2.Homework_2.line)
-            self.assertEqual(test_min, min(Homework_2.Homework_2.line))
+            test_min = Homework_2._min(Homework_2.line)
+            self.assertEqual(test_min, min(Homework_2.line))
 
         def test_03_summa(self):
-            test_sum = Homework_2._sum(Homework_2.Homework_2.line)
-            self.assertEqual(test_sum, sum(Homework_2.Homework_2.line))
+            test_sum = Homework_2._sum(Homework_2.line)
+            self.assertEqual(test_sum, sum(Homework_2.line))
 
         def test_04_multiply(self):
-            test_mult = Homework_2._mult(Homework_2.Homework_2.line)
-            self.assertEqual(test_mult, reduce(lambda x, y: x * y, (Homework_2.Homework_2.line)))
+            test_mult = Homework_2._mult(Homework_2.line)
+            self.assertEqual(test_mult, reduce(lambda x, y: x * y, (Homework_2.line)))
 
         def test_05_del(self):
-            test_delimost = Homework_2._delimost(Homework_2.Homework_2.line)
+            test_delimost = Homework_2._delimost(Homework_2.line)
             self.assertEqual(test_delimost, sum(Homework_2.line) % 3)
     else:
         def test_02_min_on_empty_file(self):
