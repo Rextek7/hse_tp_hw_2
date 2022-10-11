dd2639d70b67e15419869cffca7627186ea643a4
Rextek7 <113045888+Rextek7@users.noreply.github.com>
2022-10-11 21:35:11 +0300

    Update Tests_2.py

--- Tests_2.py
+++ Tests_2.py
@@ -8,31 +8,45 @@ import os
 import unittest
 from functools import reduce
 
+
 class TestCase(unittest.TestCase):
 
     if os.stat('Numbers.txt').st_size != 0:
 
-        def test_maximum(self):
+        def test_01_maximum(self):
             test_max = _max(line)
             self.assertEqual(test_max, max(line))
 
-        def test_minimum(self):
+        def test_02_minimum(self):
             test_min = _min(line)
             self.assertEqual(test_min, min(line))
 
-        def test_summa(self):
+        def test_03_summa(self):
             test_sum = _sum(line)
             self.assertEqual(test_sum, sum(line))
 
-        def test_multiply(self):
+        def test_04_multiply(self):
             test_mult = _mult(line)
             self.assertEqual(test_mult, reduce(lambda x, y: x * y, (line)))
 
-        def test_del(self):
+        def test_05_del(self):
             test_delimost = _delimost(line)
             self.assertEqual(test_delimost, sum(line) % 3)
     else:
-        pass
+        def test_02_min_on_empty_file(self):
+            self.assertIsNone(_min(line))
+
+        def test_01_max_on_empty_file(self):
+            self.assertIsNone(_max(line))
+
+        def test_03_summ_on_empty_file(self):
+            self.assertIsNone(_sum(line))
+
+        def test_04_mult_on_empty_file(self):
+            self.assertIsNone(_mult(line))
+
+        def test_05_del_on_empty_file(self):
+            self.assertIsNone(_delimost(line))
 
 
 
